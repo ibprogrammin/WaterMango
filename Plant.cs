a9fa16b970ea02973dedd5c094a5b744887b6844
Daniel Sevitti <daniel@seriousmonkey.ca>
2022-05-02 13:53:03 -0400

    Update Plant.cs

--- Plant.cs
+++ Plant.cs
@@ -1,5 +1,5 @@
 /***************************************************************************************************
-* Copyright 2022 (c) Daniel Sevitti <https://github.com/ibprogrammin>
+* Copyright 2006 (c) Daniel Sevitti <https://github.com/ibprogrammin>
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 * associated documentation files (the "Software"), to deal in the Software without restriction,
